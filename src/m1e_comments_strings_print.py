41d1a208301d7fc66a663bcd65fb5e1cbd2afa55
matsusy <matsusy@rose-hulman.edu>
2019-03-05 11:11:41 -0500

    DONE

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -25,10 +25,11 @@
 #
 ###############################################################################
 
-
+print('hey!!')
 print('Hello, World')
 print('hi there')
 print('one', 'two', 'buckle my shoe')
-
+print(3 + 6 * 5)
 print(3 + 9)
 print('3 + 9', 'versus', 3 + 9)
+print('3 + 9' + 'versus', 3 + 9)
